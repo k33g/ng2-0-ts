3c11edd43116a53d6ce5182307f57b5b55e53171
ph.charriere@gmail.com <ph.charriere@gmail.com>
2015-08-07 22:44:35 +0200

    Add and inject a service

--- public/js/hello.ts
+++ public/js/hello.ts
@@ -1,19 +1,26 @@
-import {Component, View, bootstrap, coreDirectives} from 'angular2/angular2';
+import { Component, View, bootstrap, coreDirectives } from 'angular2/angular2';
+import { Inject } from 'angular2/di';
+import { InformationsService } from 'js/InformationsService.ts';
 
 @Component({
-    selector: 'hello'
+  selector: 'hello',
+  appInjector: [InformationsService]
 })
 @View({
     template: `<span *ng-if="name">Hello, {{name}}!</span>`,
     directives: [coreDirectives]
 })
 export class Hello {
-    name: string = 'World';
-    constructor() {
-        setTimeout(() => {
-            this.name = 'NEW WORLD'
-        }, 2000);
-    }
+  name: string = 'World';
+  constructor(@Inject(InformationsService) informationsService) {
+    /* use service */
+    console.log("-> version", informationsService.getVersion());
+    console.log("-> applicationName", informationsService.getApplicationName());
+
+    setTimeout(() => {
+      this.name = 'NEW WORLD'
+    }, 2000);
+  }
 }
 
-bootstrap(Hello);
+bootstrap(Hello, [InformationsService]);
