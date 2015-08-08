5f588b26bb119b4282c1b482bd1bd493da8f5a6b
ph.charriere@gmail.com <ph.charriere@gmail.com>
2015-08-08 07:56:41 +0200

    Gardening

--- public/js/InformationsService.ts
+++ public/js/InformationsService.ts
@@ -1,9 +1,11 @@
 
 export class InformationsService {
-  applicationName: string = "NG2-discovery";
+  applicationName: string = "NG2-0-TS";
   version: string = "01-services";
 
-  constructor() {}
+  constructor() {
+
+  }
 
   getApplicationName() {
     return this.applicationName;
